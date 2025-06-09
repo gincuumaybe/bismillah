b068fbb22c29d0feef27e989ea8aedcebc09d12c
gincuumaybe <christherber445@gmail.com>
2025-06-09 16:39:59 +0700

    gaas

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,10 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
-
-
-
+    // public const HOME = '/dashboard';
+    public const HOME = '/user/dashboard';
 
     public function boot(): void
     {
--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -22,6 +22,8 @@ class RegistrationTest extends TestCase
         $response = $this->post('/register', [
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'phone' => '0813316478236',
+            'lokasi_kost' => 'Rungkut',
             'password' => 'password',
             'password_confirmation' => 'password',
         ]);
