8af0e0c9c11f2afa8dfa3c55f8601128eca26b90
gincuumaybe <christherber445@gmail.com>
2025-06-09 16:35:31 +0700

    cobaaa

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,8 @@ return new class extends Migration
             // $table->string('phone')->unique();
             $table->string('phone')->nullable();
             $table->string('role')->default('user');
-            $table->string('lokasi_kost');
+            // $table->string('lokasi_kost');
+            $table->string('lokasi_kost')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
