eeaf5159e25c7da76359ddb7e5f7e9b6b83c1673
gincuumaybe <christherber445@gmail.com>
2025-06-09 16:45:01 +0700

    lalalala

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,7 +18,10 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     // public const HOME = '/dashboard';
-    public const HOME = '/user/dashboard';
+    // public const HOME = '/user/dashboard';
+    public const HOME = '/penyewaan/create';
+
+
 
     public function boot(): void
     {
