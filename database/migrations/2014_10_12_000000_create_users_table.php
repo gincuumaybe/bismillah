0daa6638c5b0a13a1e0e098959f38af1606b23c9
gincuumaybe <christherber445@gmail.com>
2025-06-26 08:06:34 +0700

    tsetingg

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('image')->nullable();
             // $table->string('phone')->unique();
             $table->string('phone')->nullable();
             $table->string('role')->default('user');
